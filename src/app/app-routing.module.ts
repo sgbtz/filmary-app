37e55fd35d9983f676a614db57c24c5ba0e8b136
sgbtz <97sebastiangb@gmail.com>
2019-05-13 16:09:15 +0200

    Initial configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { routes } from './app.routes';
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
